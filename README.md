c88e311f7858d9d1773b43a612c59cb3d16ceb87
Simone Ceccarelli <ceccarellisimone1@gmail.com>
2022-11-23 18:39:21 +0100

    BaseProject

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 This is a Simple project created and maintained with the just for fun philosophy
+
+Select the extension you want by change the current branch
+
+- Layout
+- Core
+- Collection
--- src/cytoscape-extensions-boilerplate.ts
+++ src/cytoscape-extensions-boilerplate.ts
@@ -1,14 +1,16 @@
-import Cosmos from "./algorithm/layout";
-
-export * from "./interfaces";
-
 // registers the extension on a cytoscape lib ref
 export default function register(cytoscape) {
   if (!cytoscape) {
     return;
   } // can't register if cytoscape unspecified
 
-  cytoscape("layout", "cosmos", Cosmos); // register with cytoscape.js
+  /**
+   * type = core | layout | collection
+   * name = the name of the functionality
+   * mainFunction = function that if performed, performs the tasks we want
+   *
+   * cytoscape(type, name, mainFunction);
+   *  */
 }
 
 if (typeof window.cytoscape !== "undefined") {
